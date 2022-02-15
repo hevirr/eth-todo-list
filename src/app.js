bcceb0fa9e506d5afc91a4a435e05d51d1d57c52
Anton <hvr.blackswan@gmail.com>
2022-02-16 01:20:15 +0700

    tasks completion

--- src/app.js
+++ src/app.js
@@ -15,26 +15,19 @@ App = {
     } else {
       window.alert("Please connect to Metamask.")
     }
-    // Modern dapp browsers...
     if (window.ethereum) {
       window.web3 = new Web3(ethereum)
       try {
-        // Request account access if needed
         await ethereum.enable()
-        // Acccounts now exposed
         web3.eth.sendTransaction({/* ... */})
       } catch (error) {
-        // User denied account access...
       }
     }
-    // Legacy dapp browsers...
     else if (window.web3) {
       App.web3Provider = web3.currentProvider
       window.web3 = new Web3(web3.currentProvider)
-      // Acccounts always exposed
       web3.eth.sendTransaction({/* ... */})
     }
-    // Non-dapp browsers...
     else {
       console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
@@ -99,6 +92,13 @@ App = {
     App.setLoading(true);
     const content = $('#newTask').val();
     await App.todoList.createTask(content)
+    window.location.reload()
+  },
+
+  toggleCompleted: async (e) => {
+    App.setLoading(true)
+    const taskId = e.target.name;
+    await App.todoList.toggleCompleted(taskId)
     window.location.reload()
   },
 
--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -1,6 +1,6 @@
 const TodoList = artifacts.require('./TodoList.sol')
 
-contract('TodoList', (accounts) => {
+contract('TodoList', () => {
   before(async () => {
     this.todoList = await TodoList.deployed()
   })
@@ -17,7 +17,7 @@ contract('TodoList', (accounts) => {
     const taskCount = await this.todoList.taskCount()
     const task = await this.todoList.tasks(taskCount)
     assert.equal(task.id.toNumber(), taskCount.toNumber())
-    assert.equal(task.content, 'This string is written by me')
+    assert.equal(task.content, 'Default task')
     assert.equal(task.completed, false)
     assert.equal(taskCount.toNumber(), 1)
   })
@@ -30,5 +30,14 @@ contract('TodoList', (accounts) => {
     assert.equal(event.id.toNumber(), 2)
     assert.equal(event.content, 'A new task')
     assert.equal(event.completed, false)
+  })
+
+  it('toggles task completion', async () => {
+    const result = await this.todoList.toggleCompleted(1)
+    const task = await this.todoList.tasks(1)
+    assert.equal(task.completed, true)
+    const event = result.logs[0].args
+    assert.equal(event.id.toNumber(), 1)
+    assert.equal(event.completed, true)
   })
 })
